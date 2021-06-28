3e7adc4f4985aaddaa6a650e44ff8f36a602ce34
Ekaterina <katena.trostina@mail.ru>
2021-06-28 19:24:50 +0300

    test-task

--- dist/js/index.js
+++ dist/js/index.js
@@ -8,8 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         lamps: 0, 
         chandeliers: 0    
     };
-    let cloth,
-    manufacturer = 1;
+    let manufacturer;
 
     Object.keys(changingValue).forEach(key => {
         console.log(document.getElementById(key));
@@ -23,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function setTextAndCalcResult(name, value){
         setText(name, value);
-        result.textContent = calcTotal();
+        result.textContent = calcTotal(manufacturer);
     };
 
     decrementSquare.addEventListener('click', () => {
@@ -58,11 +57,22 @@ document.addEventListener('DOMContentLoaded', () => {
         setTextAndCalcResult('chandeliers', 1);
     });
 
+    const dPremiun = { id: 0, name:'D-Premium', cost: 580};
+    const clipso = { id: 1, name: 'Clipso', cost: 2390};
+    const cerutti = { id: 2, name: 'Cerutti', cost: 2550};
+    const classic = { id: 3, name: 'Classic', cost: 140};
+    const pongs = { id: 4, name: 'Pongs', cost: 390};
+    const evolution = { id: 5, name: 'Evolution', cost: 290};
+    const teqtum = { id: 6, name: 'Teqtum', cost: 440};
+    const premium = { id: 7, name: 'Premium', cost: 190};
+    const lumfer = { id: 8, name: 'Lumfer', cost: 660};
+    const coldStretch = { id: 9, name: 'Cold Stretch', cost: 390};
+
     let manufacturers = {
-        tissue: ['D-Premium', 'Clipso', 'Cerutti'],
-        matt: ['Classic','Pongs', 'Evolution', 'Teqtum', 'Premium', 'Lumfer','Cold Stretch'],
-        glossy: ['Classic', 'Pongs', 'Evolution', 'Premium', 'Lumfer'],
-        satin: ['Classic', 'Pongs', 'Evolution', 'Premium', 'Lumfer'],
+        tissue: [dPremiun, clipso, cerutti],
+        matt: [classic, pongs, evolution, teqtum, premium, lumfer,coldStretch],
+        glossy: [classic, pongs, evolution, teqtum, lumfer],
+        satin: [classic, pongs, evolution, teqtum, lumfer],
     };
 
     let materials = [
@@ -81,15 +91,17 @@ document.addEventListener('DOMContentLoaded', () => {
         for(let i = 0; i < items.length; i++){
             const item = items[i];
             const htmlItem = document.createElement("LI");
-            const textnode = document.createTextNode(item);
+            const textnode = document.createTextNode(item['name']);
             htmlItem.appendChild(textnode);
             htmlItem.classList.add('calc__manufacturer-name');
             htmlItem.addEventListener('click', () => {
-                if (prevManufacturer !== undefined) {
+                if (prevManufacturer) {
                     prevManufacturer.classList.remove('active-btn');
                 }
                 htmlItem.classList.add('active-btn');
+                manufacturer = item['cost'];
                 prevManufacturer = htmlItem;
+                result.textContent = calcTotal(manufacturer);
             })
 
             list.appendChild(htmlItem);
@@ -107,62 +119,40 @@ document.addEventListener('DOMContentLoaded', () => {
             htmlItem.appendChild(textnode);
             htmlItem.classList.add('calc__type-texture-name');
             htmlItem.addEventListener('click', () => {
-                if (prevMaterial !== undefined) {
+                prevManufacturer = undefined;
+                manufacturer = undefined;
+                if (prevMaterial) {
                     prevMaterial.classList.remove('active-btn');
                 }
                 htmlItem.classList.add('active-btn');
                 prevMaterial = htmlItem;
                 let id = item['id'];
                 showList(manufacturers[id]);
+                result.textContent = calcTotal(manufacturer);
             })
             list.appendChild(htmlItem);
         };
     }
     showMaterials(materials);
 
-    
-    
     //calc
     const result = document.querySelector('.calc__total-value');
-    
-    function calcTotal() {
-        // if (!cloth || !manufacturer || !squa || !angle || !lamp || !chandelier) {
-        //     result.textContent = '___';
-        //     return;
-        // }
 
-        // manufacturer *
+    function calcTotal(manufacturer) {
+        const squa = changingValue['squa'],
+              angle = changingValue['angles'],
+              lamp = changingValue['lamps'],
+              chandelier = changingValue['chandeliers'];
 
-        return(changingValue['squa'] + 180 * changingValue['angles'] + 290 * changingValue['lamps'] + 450 * changingValue['chandeliers'] + '₽');
+        if (!manufacturer || !squa || !angle || !lamp || !chandelier) {
+            return '___';
+        }
+
+        return(manufacturer * squa + 180 * angle + 290 * lamp + 450 * chandelier + '₽');
 
     }
 
-    calcTotal();
-
-    // function getStaticInfo(parentSelector, activeClass){
-    //     const elements = document.querySelectorAll(`${parentSelector} li`);
-
-    //     elements.forEach(elem => {
-    //         elem.addEventListener('click', (e) => {
-    //             if(e.target.getAttribute('id')){
-    //                 cloth = e.target.getAttribute('id');
-    //             }
-    
-    //             console.log(cloth);
-    
-    //             elements.forEach(elem => {
-    //                 elem.classList.remove(activeClass);
-    //             });
-    
-    //             e.target.classList.add(activeClass);
-    //         });
-    //     });
-
-    // }
+    result.textContent = calcTotal(undefined);
 
 
-    // getStaticInfo('#type-texture', 'active-btn');
-    // getStaticInfo('#manufacturer', 'active-btn');
-    
-    // getStaticInfo
-})
+});
